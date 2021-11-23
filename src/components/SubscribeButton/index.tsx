dd30c619ec8e955a71da6d3e7aefd5e97043dae3
Gustavo Galupo <ggalupo2@hotmail.com>
2021-11-22 21:29:23 -0300

    Prevent users with active subscription to subscribe again

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,42 +1,48 @@
-import { signIn, useSession } from 'next-auth/client'
-import { api } from '../../services/api'
-import { getStripeJs } from '../../services/stripe-js'
+import { signIn, useSession } from "next-auth/client";
+import router, { useRouter } from "next/router";
+import { api } from "../../services/api";
+import { getStripeJs } from "../../services/stripe-js";
 
-import styles from './styles.module.scss'
+import styles from "./styles.module.scss";
 
 interface SubscribeButtonProps {
-    priceId: string
+  priceId: string;
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
-    const [session] = useSession()
+  const [session] = useSession();
+  const router = useRouter();
 
-    async function handleSubscribe() {
-        if (!session) {
-            signIn('github')
-            return
-        }
-
-        try {
-            const response = await api.post('/subscribe')
-
-            const { sessionId } = response.data
-
-            const stripe = await getStripeJs()
-            await stripe.redirectToCheckout({ sessionId })
-
-        } catch(err) {
-            alert(err.message)
-        }
+  async function handleSubscribe() {
+    if (!session) {
+      signIn("github");
+      return;
     }
 
-    return (
-        <button
-            type="button"
-            className={styles.subscribeButton}
-            onClick={handleSubscribe}
-        >
-            Subscribe now
-        </button>
-    )
+    if (session.activeSubscription) {
+      router.push("/");
+      return;
+    }
+
+    try {
+      const response = await api.post("/subscribe");
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+      await stripe.redirectToCheckout({ sessionId });
+    } catch (err) {
+      alert(err.message);
+    }
+  }
+
+  return (
+    <button
+      type="button"
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+    >
+      Subscribe now
+    </button>
+  );
 }
