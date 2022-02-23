a6bbd1b778ab08baddc15807942953fef33c3d21
Gustavo Galupo <ggalupo2@hotmail.com>
2022-02-23 02:32:41 -0300

    Home page tests

---
+++ src/tests/pages/Home.test.tsx
@@ -0,0 +1,39 @@
+import { render, screen } from "@testing-library/react";
+import { mocked } from "jest-mock";
+
+import { stripe } from "../../services/stripe";
+import Home, { getStaticProps } from "../../pages";
+
+jest.mock("next/router");
+jest.mock("next-auth/client", () => {
+  return {
+    useSession: () => [null, false],
+  };
+});
+jest.mock("../../services/stripe");
+
+describe("Home page", () => {
+  it("should render correctly", () => {
+    render(<Home formattedPrice="$20.00" />);
+
+    expect(screen.getByText("for $20.00/month.")).toBeInTheDocument();
+  });
+
+  it("should get formatted stripe price from getStaticProps", async () => {
+    const retrieveStripePriceMocked = mocked(stripe.prices.retrieve);
+
+    retrieveStripePriceMocked.mockResolvedValueOnce({
+      unit_amount: 2000,
+    } as any);
+
+    const response = await getStaticProps({});
+
+    expect(response).toEqual(
+      expect.objectContaining({
+        props: {
+          formattedPrice: "$20.00",
+        },
+      })
+    );
+  });
+});
