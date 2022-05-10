a06b71e34732b90af70bd4cf48549152b7c2f916
Gustavo Galupo <ggalupo2@hotmail.com>
2022-05-10 01:21:01 -0300

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,21 +1,23 @@
-module.exports = {
-    testPathIgnorePatterns: ["/node_modules/", "/.next/"],
-    setupFilesAfterEnv: [
-        "<rootDir>/src/tests/setupTests.ts"
-    ],
-    transform: {
-        "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest"
-    },
-    moduleNameMapper: {
-        "\\.(scss|css|sass)$": "identity-obj-proxy"
-    },
-    testEnvironment: 'jsdom',
-    collectCoverage: true,
-    collectCoverageFrom: [
-        "src/**/*.tsx",
-        "!src/**/*.spec.tsx",
-        "!src/pages/_app.tsx",
-        "!src/pages/_document.tsx",
-    ],
-    coverageReporters: ["lcov", "json"]
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+const customJestConfig = {
+  setupFilesAfterEnv: [
+    "<rootDir>/src/tests/setupTests.ts"
+],
+  moduleDirectories: ['node_modules', '<rootDir>/'],
+  testEnvironment: 'jest-environment-jsdom',
+  collectCoverage: true,
+  collectCoverageFrom: [
+      "src/**/*.tsx",
+      "!src/**/*.spec.tsx",
+      "!src/pages/_app.tsx",
+      "!src/pages/_document.tsx",
+  ],
+  coverageReporters: ["lcov", "json"]
 }
+
+module.exports = createJestConfig(customJestConfig)
