f911a006f69cf90c80f2403ac8977ac207dfb61b
Gustavo Galupo <ggalupo2@hotmail.com>
2022-05-23 22:56:20 -0300

    Update tests for SignInButton component

--- src/components/SignInButton/SignInButton.test.tsx
+++ src/components/SignInButton/SignInButton.test.tsx
@@ -1,15 +1,20 @@
 import { render, screen } from "@testing-library/react";
 import { mocked } from "jest-mock";
-import { useSession } from "next-auth/client";
+import { useSession } from "next-auth/react";
+
+import { type useSessionMockedReturn } from "../../tests";
 
 import { SignInButton } from ".";
 
-jest.mock("next-auth/client");
+jest.mock("next-auth/react");
 
 describe("SignInButton component", () => {
   it("should render correctly when user is not logged in", () => {
     const useSessionMocked = mocked(useSession);
-    useSessionMocked.mockReturnValueOnce([null, false]);
+    useSessionMocked.mockReturnValueOnce({
+      data: null,
+      status: "unauthenticated",
+    } as useSessionMockedReturn);
 
     render(<SignInButton />);
 
@@ -18,14 +23,14 @@ describe("SignInButton component", () => {
 
   it("should render correctly when user is logged in", () => {
     const useSessionMocked = mocked(useSession);
-    useSessionMocked.mockReturnValueOnce([
-      {
+    useSessionMocked.mockReturnValueOnce({
+      data: {
         user: {
           name: "John Doe",
         },
       },
-      false,
-    ]);
+      status: "authenticated",
+    } as useSessionMockedReturn);
     render(<SignInButton />);
 
     expect(screen.getByText("John Doe")).toBeInTheDocument();
